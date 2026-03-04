890cf8349af8f1bb04b64b16e7f7c12dd70b1a50
karleaabha-byte <karle.aabha@gmail.com>
2026-03-04 23:25:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,32 +6,20 @@ import base64
 app = Flask(__name__)
 
 HF_TOKEN = os.environ.get("HF_TOKEN")
-HF_API_URL = "https://api-inference.huggingface.co/models/google/vit-base-patch16-224"
+HF_API_URL = "https://api-inference.huggingface.co/models/umm-maybe/AI-image-detector"
 
 headers = {
     "Authorization": f"Bearer {HF_TOKEN}"
 }
 
-def query_hf_model(image_bytes):
-    try:
-        response = requests.post(
-            HF_API_URL,
-            headers=headers,
-            data=image_bytes,
-            timeout=30
-        )
-
-        if response.status_code != 200:
-            return {
-                "error": f"HF Error {response.status_code}",
-                "details": response.text
-            }
-
-        return response.json()
-
-    except Exception as e:
-        return {"error": str(e)}
-
+def detect_ai(image_bytes):
+    response = requests.post(
+        HF_API_URL,
+        headers=headers,
+        data=image_bytes,
+        timeout=30
+    )
+    return response.json()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -41,11 +29,9 @@ def index():
     if request.method == "POST":
         image_file = request.files.get("image")
 
-        if not image_file:
-            result = {"error": "No file uploaded."}
-        else:
+        if image_file:
             image_bytes = image_file.read()
-            result = query_hf_model(image_bytes)
+            result = detect_ai(image_bytes)
             image_data = base64.b64encode(image_bytes).decode("utf-8")
 
     return render_template("index.html", result=result, image_data=image_data)
